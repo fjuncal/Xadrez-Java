80122380d5c6f1a1282ee602b8d8fbbf6cd7995b
Fellipe Juncal <fjuncal7@gmail.com>
2020-02-14 00:28:59 -0300

    Primeira Classe: Posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 	}
 
 }
